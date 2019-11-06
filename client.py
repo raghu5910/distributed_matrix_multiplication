31814053527faa8e25daa699d86952953828cf44
Raghu Paredla <raghuram5910@gmail.com>
2019-11-06 20:07:34 +0530

    unnecessary code cleaned

--- client.py
+++ client.py
@@ -9,68 +9,24 @@ import numpy as np
 
 class ClientClass:
     def __init__(self, port_number):
-        self.array = None
-        self.block_count = None
         self.block_size = None
-        self.servers = None
-        self.machine_number = 0
         self.port_number = port_number
-        self.machine_count = 0
+        self.block_size = None
 
-    def get_result_matrix(self):
-        result = []
-        for element in self.array:
-            temp = None
-            for arr in element:
-                if temp is None:
-                    temp = arr.get_c_matrix()
-                    print(temp)
-                else:
-                    matrix = arr.get_c_matrix()
-                    temp = numpy.append(temp, matrix, axis=1)
-            result.append(temp)
-        # result = numpy.append(*result, axis=0)
-        print(temp)
+    def create_remote_objects(self, remote_urls):
+        remotes = []
+        for remote_url in remote_urls:
+            url = remote_url + str(self.port_number)
+            matrix = Pyro4.Proxy(url)
+            remotes.append(matrix)
+        return remotes
 
-    def run(self, machine_number, generator_from, generator_to):
-        self.servers = [
-            "PYRO:matrix@localhost:",
-            # "PYRO:matrix@someotherserver.org:",
-        ]
-        machine_number = int(machine_number)
-        self.block_size = int(array_size / machine_number)
-        self.block_count = int(array_size / self.block_size)
-        # numpy.set_printoptions(threshold=numpy.nan)
-        matrices = self.generate_matrices(array_size, generator_from, generator_to)
-        matrices = (self.split_matrix(matrices[0]), self.split_matrix(matrices[1]))
-        # Pyro4.config.SERIALIZER = "pickle"
-        self.cannons_algorithm(matrices)
-        self.get_result_matrix()
-        print("--- %s seconds ---" % (time.time() - start_time))
-
-    def split_matrix(self, input_matrix):
-        result_matrix = []
-        for x in range(0, self.block_count):
-            temporary_array = []
-            for y in range(0, self.block_count):
-                x_value = int(self.block_size * x)
-                x_second_value = int(self.block_size * (x + 1))
-                y_value = int(self.block_size * y)
-                y_second_value = int(self.block_size * (y + 1))
-                temporary_array.append(
-                    input_matrix[x_value:x_second_value, y_value:y_second_value]
-                )
-            result_matrix.append(temporary_array)
-        return result_matrix
-
-    def generate_matrices(self, array_size, generator_from, generator_to):
-        matrix_a = numpy.matlib.matrix(
-            self.generate_matrix(array_size, generator_from, generator_to)
-        )
-        matrix_b = numpy.matlib.matrix(
-            self.generate_matrix(array_size, generator_from, generator_to)
-        )
-        return matrix_a, matrix_b
+    def matrix_generation(self, matrix_size, generate_to):
+        """Generating random matrices of size (matrix_size, matrix_size)"""
+        matrix_A = np.random.randint(generate_to, size=(matrix_size, matrix_size))
+        matrix_B = np.random.randint(generate_to, size=(matrix_size, matrix_size))
+        matrices = (matrix_A, matrix_B)
+        return matrices
 
     def skew_matrix(self, matrices):
         matrix_A, matrix_B = matrices
@@ -149,7 +105,7 @@ class ClientClass:
                     remotes[remote_num].multiply()
                     remote_num = remote_num + 1
             t = t + 1
-        matrix = np.zeros(shape=(array_size, array_size))
+        matrix = np.zeros(shape=(matrix_size, matrix_size))
         remote_num = 0
         for i in range(0, self.block_size):
             for j in range(0, self.block_size):
@@ -162,93 +118,18 @@ class ClientClass:
 
         return matrix
 
-    def matrix_generation(self, matrix_size, generate_to):
-        """Generating random matrices of size (matrix_size, matrix_size)"""
-        matrix_A = np.random.randint(generate_to, size=(matrix_size, matrix_size))
-        matrix_B = np.random.randint(generate_to, size=(matrix_size, matrix_size))
-        matrices = (matrix_A, matrix_B)
-        return matrices
-
-    @staticmethod
-    def generate_matrix(array_size, generate_from, generator_to):
-        result = ""
-        for _ in range(0, array_size):
-            for _ in range(0, array_size):
-                result += str(random.randint(0, generator_to)) + ","
-            result = result[:-1] + ";"
-        return result[:-1]
-
-    def cannons_algorithm(self, matrices):
-        self.array = []
-        self.set_initial_matrices(matrices)
-        self.skew(self.block_count)
-        for _ in range(0, self.machine_number):
-            for element in self.array:
-                for value in element:
-                    value.multiply()
-                    print(value.get_c_matrix())
-            self.shift_a_matrix_left()
-            self.shift_b_matrix_up(self.block_count)
-
-    def set_initial_matrices(self, matrices):
-        for x in range(0, self.block_count):
-            temp_array = []
-            for y in range(0, self.block_count):
-                url = self.servers[self.machine_count % 2] + str(self.port_number)
-                matrix = Pyro4.Proxy(url)
-                matrix.clear_c_matrix()
-                matrix.set_matrix_a(matrices[0][x][y])
-                matrix.set_matrix_b(matrices[1][x][y])
-                # print(matrix.get_matrix_a())
-                # matrix.multiply()
-                # print(matrix.get_c_matrix())
-                temp_array.append(matrix)
-                self.machine_count += 1
-                if self.machine_count % 2 == 0:
-                    self.port_number += 1
-            self.array.append(temp_array)
-            # print(self.array)
-
-    def skew(self, vector_length):
-        for i in range(1, vector_length):
-            current_element = self.array[0][i].get_matrix_b()
-            for element in reversed(self.array):
-                temp = element[i].get_matrix_b()
-                element[i].set_matrix_b(current_element)
-                current_element = temp
-        for i in range(1, vector_length):
-            current_element = self.array[i][0].get_matrix_a()
-            for el in reversed(self.array[i]):
-                temp = el.get_matrix_a()
-                el.set_matrix_a(current_element)
-                current_element = temp
-
-    def shift_a_matrix_left(self):
-        for element in self.array:
-            current_element = element[0].get_matrix_a()
-            for el in reversed(element):
-                temp = el.get_matrix_a()
-                el.set_matrix_a(current_element)
-                current_element = temp
-
-    def shift_b_matrix_up(self, block_count):
-        for i in range(0, block_count):
-            current_element = self.array[0][i].get_matrix_b()
-            for element in reversed(self.array):
-                temp = element[i].get_matrix_b()
-                element[i].set_matrix_b(current_element)
-                current_element = temp
-
 
 if __name__ == "__main__":
     Pyro4.config.SERIALIZER = "pickle"
     start_time = time.time()
-    machineNumber = float(sys.argv[1])
-    machineNumber = math.sqrt(machineNumber)
-    array_size = int(sys.argv[2])
-    if machineNumber.is_integer() and array_size % machineNumber == 0:
+    matrix_size = int(sys.argv[1])
+    machineNumber = float(sys.argv[2])
+    generate_to = int(sys.argv[3])
+    remote_urls = ["PYRO:matrix@localhost:"]
+    if machineNumber.is_integer() and matrix_size % math.sqrt(machineNumber) == 0:
         client = ClientClass(9601)
-        client.run(machineNumber, int(sys.argv[3]), int(sys.argv[4]))
+        remotes = client.create_remote_objects(remote_urls)
+        client.start(matrix_size, machineNumber, generate_to, remotes)
     else:
         print("Square root of machines count must be an integer!")
 
